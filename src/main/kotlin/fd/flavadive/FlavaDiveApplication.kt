331fd6a2f4eb6928401dc97f1d5c594336b9bbd3
박현진 <hyunjin@petch.com>
2025-01-22 07:06:13 +0900

    Initial commit

---
+++ src/main/kotlin/fd/flavadive/FlavaDiveApplication.kt
@@ -0,0 +1,11 @@
+package fd.flavadive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlavaDiveApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlavaDiveApplication>(*args)
+}
---
+++ src/test/kotlin/fd/flavadive/FlavaDiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package fd.flavadive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlavaDiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
